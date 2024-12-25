42233d261725bf79a79579d308d7c477e13c8873
Deepak karki <ideepakkarki@gmail.com>
2024-12-25 09:45:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
-//your JS code here. If required.
-let inputs = document.querySelectorAll(".code");
+ let inputs = document.querySelectorAll(".code");
 
-inputs.forEach((input, i) => {
-  input.addEventListener("input", (e) => {
-    let val = e.target.value;
-    if (val.length > 0 && i < inputs.length - 1) {
-      inputs[i + 1].focus();
-    }
-  });
+      inputs.forEach((input, i) => {
+        input.addEventListener("input", (e) => {
+          let val = e.target.value;
+          if (val.length > 0 && i < inputs.length - 1) {
+            inputs[i + 1].focus(); // Move focus to next input
+          }
+        });
 
-  input.addEventListener("keydown", (e) => {
-    if (e.key === "Backspace" && !e.target.value && i > 0) {
-      inputs[i - 1].focus();
-    }
-  });
-});
+        input.addEventListener("keydown", (e) => {
+          if (e.key === "Backspace" && !e.target.value && i > 0) {
+            inputs[i - 1].focus(); // Move focus to previous input
+          }
+        });
 
-console.log(inputs.length);
+        input.addEventListener("focus", () => {
+          input.select(); // Highlight the text inside when the input is focused
+        });
+      });
+
+      console.log(inputs.length);
